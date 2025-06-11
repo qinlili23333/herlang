c0343bfd96fcd5a9306c5e6c246a4c841f2519cd
lzjyzq2 <lzjyzq2@hotmail.com>
2025-06-12 00:17:11 +0800

    Fix: 修复在Windows平台默认字符集非UTF8时读取代码不正确

--- src/HerlangExt.java
+++ src/HerlangExt.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class HerlangExt {
@@ -351,7 +352,7 @@ public class HerlangExt {
  	}
 
 	private static String[] readLines(File file) throws Exception {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
 		ArrayList<String> lines = new ArrayList<String>();
 		for(String ln = reader.readLine(); ln != null; ln = reader.readLine()) {
 			lines.add(ln);
